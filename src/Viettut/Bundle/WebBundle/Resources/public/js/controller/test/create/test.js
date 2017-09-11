1eb5d2fe8213f3f3171eb28926a444543e14187a
Viettut <giangyenbai@gmail.com>
2017-09-11 22:53:27 +0700

    add new test

--- src/Viettut/Bundle/WebBundle/Resources/public/js/controller/test/create/test.js
+++ src/Viettut/Bundle/WebBundle/Resources/public/js/controller/test/create/test.js
@@ -16,6 +16,7 @@ angular
         $scope.serverParameters = {};
         $scope.uploaded = false;
         $scope.uploadError = false;
+        $scope.choices = [{'value': ''}];
 
         $scope.addOption = function() {
             if (!$scope.option) {
@@ -27,82 +28,14 @@ angular
             $scope.option = '';
         };
 
-        $scope.removeTag = function(tag) {
-            var index = $scope.courseTags.indexOf(tag);
-            $scope.courseTags.splice(index, 1);
+        $scope.addNewChoice = function() {
+            $scope.choices.push({'value': ''});
+            console.log($scope.choices);
         };
 
-        $scope.create = function () {
-            var data = {
-                title: $scope.title,
-                imagePath: $scope.image,
-                introduce: $scope.introduce,
-                courseTags: $scope.courseTags,
-                published: $scope.published
-            };
-
-            // start progress
-            $scope.laddaLoading = true;
-
-            $http.post(config.API_URL + 'courses', data).
-            then(
-                function(response){
-                    $scope.laddaLoading = false;
-                    if(response.status == 201) {
-                        $scope.course = response.data;
-                        $window.location.href = config.BASE_URL + 'courses/' + $scope.course.token + '/add-chapter';
-                    }
-                },
-                function(response) {
-                    $scope.laddaLoading = false;
-                    $scope.addError(response.data.message);
-                });
-        };
-
-
-        $scope.uploadFiles = function (file) {
-            $scope.f = file;
-            if (file && !file.$error) {
-                file.upload = Upload.upload({
-                    url: config.API_URL + 'courses/uploads?XDEBUG_SESSION_START=1',
-                    file: file
-                });
-
-                file.upload.then(function (response) {
-                    $timeout(function () {
-                        file.result = response.data;
-                        $scope.image = response.data;
-                        $scope.uploaded = true;
-                        $scope.uploadError = false;
-                    });
-                }, function (response) {
-                    if (response.status > 0) {
-                        $scope.uploadError = true;
-                        $scope.uploadErrorMsg = response.status + ': ' + response.data;
-                    }
-                });
-            }
-            else {
-                $scope.uploadError = true;
-                $scope.uploadErrorMsg = 'Image\'s max height is 1000px and max size is 1MB';
-            }
-        };
-        
-
-        $scope.addError = function(message) {
-            var html = '<div class="alert alert-danger alert-dismissable">' +
-                '    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' +
-                message +
-                '</div>';
-            angular.element($('form.form-horizontal')).before(html);
-        };
-
-        $scope.addInfo = function(message) {
-            var html = '<div class="alert alert-success alert-dismissable">' +
-                '    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' +
-                message +
-                '</div>';
-            angular.element($('form.form-horizontal')).before(html);
+        $scope.removeChoice = function() {
+            var lastItem = $scope.choices.length-1;
+            $scope.choices.splice(lastItem);
         };
     });
 
